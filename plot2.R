cfc10e1963d6cf78351b0638c584ec0758f9ccfc
Chava Gemini Krishna <chava.geminikrishna@gmail.com>
2020-05-24 00:03:54 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,5 +4,5 @@ file=na.omit(file)
 file$Date=as.Date(as.character(file$Date),format = "%d/%m/%Y")
 plots=filter(file, Date >= "2007-02-01" & Date <= "2007-02-02")
 datetime=strptime(paste(plots$Date,plots$Time),format =  "%Y-%m-%d %H:%M:%S" )
-plot(datetime,plots$Global_active_power,type = "l",ylab="Global Active Power (kilowatts)")
+plot(datetime,plots$Global_active_power,type = "l",ylab="Global Active Power (kilowatts)",, xlab="")
 
