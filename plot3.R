762cae570d525623f03c38efd61c6bb3f2fb0e77
Chava Gemini Krishna <chava.geminikrishna@gmail.com>
2020-05-24 00:47:58 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -9,3 +9,5 @@ points(datetime,plots$Sub_metering_2,type="l",col="red")
 points(datetime,plots$Sub_metering_3,type="l",col="blue")
 legend("topright",lwd=1,col =c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
