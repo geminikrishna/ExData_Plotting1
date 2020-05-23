2391b8b7cd9f6192baf3b5a296f96fb16bd1f4ca
Chava Gemini Krishna <chava.geminikrishna@gmail.com>
2020-05-24 00:22:22 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+install.packages("dplyr")
+file=read.csv("household_power_consumption.txt",sep=";",na.strings = "?")
+file=na.omit(file)
+file$Date=as.Date(as.character(file$Date),format = "%d/%m/%Y")
+plots=filter(file, Date >= "2007-02-01" & Date <= "2007-02-02")
+datetime=strptime(paste(plots$Date,plots$Time),format =  "%Y-%m-%d %H:%M:%S" )
+plot(datetime,plots$Sub_metering_1,type="l",ylab="Energy sub metering", xlab="",col="black")
+points(datetime,plots$Sub_metering_2,type="l",col="red")
+points(datetime,plots$Sub_metering_3,type="l",col="blue")
+legend("topright",lwd=1,col =c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
