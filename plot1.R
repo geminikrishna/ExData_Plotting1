c855e2801daf0c51aff22c45ef48843534a75962
Chava Gemini Krishna <chava.geminikrishna@gmail.com>
2020-05-23 23:32:54 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+install.packages("dplyr")
+file=read.csv("household_power_consumption.txt",sep=";",na.strings = "?")
+file=na.omit(file)
+file$Date=as.Date(as.character(file$Date),format = "%d/%m/%Y")
+plots=filter(file, Date >= "2007-02-01" & Date <= "2007-02-02")
+hist(plots$Global_active_power,col = "red",xlab="Global Active Power (kilowatts)",main = "Global Active Power")
+
