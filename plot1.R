2a75b427e1e1d45b263de1fd1286502708234cc0
Chava Gemini Krishna <chava.geminikrishna@gmail.com>
2020-05-24 00:48:45 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,3 +5,6 @@ file$Date=as.Date(as.character(file$Date),format = "%d/%m/%Y")
 plots=filter(file, Date >= "2007-02-01" & Date <= "2007-02-02")
 hist(plots$Global_active_power,col = "red",xlab="Global Active Power (kilowatts)",main = "Global Active Power")
 
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
+
